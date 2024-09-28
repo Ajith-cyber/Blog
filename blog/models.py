838050a48547895615ed6100f60835446372ea7d
Ajith-cyber <ajithuser12@gmail.com>
2024-09-28 16:13:50 +0530

    database

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=50)
-    text=models.TextField()
+    content=models.TextField()
     img_url=models.URLField(null=True)
+    created_at= models.DateTimeField(auto_now_add=True)
 
 # that only prints non-sensitive information
     def __str__(self):
